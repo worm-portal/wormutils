f61a9a394065cc1380933ce0b6b52806f233acb9
Grayson Boyer <gmboyer@asu.edu>
2025-06-14 22:09:42 +0000

    added new conversion module

--- build/lib/WORMutils/__init__.py
+++ build/lib/WORMutils/__init__.py
@@ -7,3 +7,4 @@ from .isnotebook import isnotebook
 from .Error_Handler import Error_Handler
 from .get_colors import get_colors
 from .can_connect_to import can_connect_to
+from .convert import assign_worm_db_col_dtypes
