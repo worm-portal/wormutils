420073699ef51a41660a16a1ca2cceddf4eb9845
Grayson Boyer <gmboyer@asu.edu>
2025-06-17 02:20:17 +0000

    updated the package loading function to be compatible with AqOrg

--- WORMutils/import_package_resources.py
+++ WORMutils/import_package_resources.py
@@ -3,16 +3,20 @@ from io import StringIO
 import pandas as pd
 
 
-def import_package_file(pkg_name, filename):
+def import_package_file(pkg_name, filename, import_csv_as_df=True, as_file=False):
     ref = importlib_resources.files(pkg_name) / filename
-    with importlib_resources.as_file(ref) as path:
-        with open(path, 'r') as f:
-            content = f.read()
 
-    # convert a csv (as text) into a pandas csv
-    if filename[-4:] == ".csv":
-        content = pd.read_csv(StringIO(content), sep=",")
+    if as_file:
+        # Return the context manager for use with 'with' statement
+        return importlib_resources.as_file(ref)
+    else:
+        # Read content as string
+        with importlib_resources.as_file(ref) as path:
+            with open(path, 'r') as f:
+                content = f.read()
+                
+        # Convert CSV to pandas DataFrame if requested
+        if filename[-4:] == ".csv" and import_csv_as_df:
+            content = pd.read_csv(StringIO(content), sep=",")
     
     return content
-
-    
